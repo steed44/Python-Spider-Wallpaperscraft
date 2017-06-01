4713c5bcd96f7ea2c5cf657d252625a377f4544d
steed <549847646@qq.com>
2017-06-01 18:24:02 +0800

    添加了多线程
    多线程下载

--- 爬取壁纸1.py
+++ 爬取壁纸1.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import threading
+import time
 
 '''该程序可以爬取https://wallpaperscraft.com/all的类型为“all”的壁纸，用户可以在下面更改一些
 自己需要的设置'''
@@ -12,6 +13,9 @@ starPage = 47            #设置爬取起始页（首页是1）
 endPage = 70             #设置爬取终止页（最终页可以到网站查看）
 resolutio = "2560x1080" #设置你需要的分辨率（注意乘号‘x’的格式），要保证分辨率是壁纸网站所用有的
 kv = {'user-agent':'Mozilla/5.0'}  #设置模拟的浏览器
+lock = 1                #锁，保证栈每次只能有一个访问
+mainPageList = []       #一级页面栈
+wallPaperList = []     #二级页面栈
 
 #获取页面内容
 def getHTMLText(url):
@@ -25,24 +29,58 @@ def getHTMLText(url):
         return ""
 
 #解析页面,提取图片地址
-def parsePage(html):
-    soup = BeautifulSoup(html,'html.parser')
-    ilt = soup.find_all('a',target="_blanck")
-    #print(ilt[2])
-    for i in range(1,21):
-        #print(ilt[i].get('href'))
-        parseDownAdd(ilt[i].get('href'))
+def parsePage():
+    while True:
+        n = 5
+        html = ""
+        print("1")
+        while len(mainPageList) == 0:
+            time.sleep(50)
+            
+        url = mainPageList.pop()
+        print("11"+url)
+        
+        while html == "":
+            html = getHTMLText(url)
+            --n
+            if(n == 0):
+                mainPageList.inster(1,url)
+                break
+        soup = BeautifulSoup(html,'html.parser')
+        ilt = soup.find_all('a',target="_blanck")
+       
+        for i in range(1,21):
+            wallPaperList.append('https:' + ilt[i].get('href'))
+            print('https:' + ilt[i].get('href'))
     
 
 #解析子页，提取高清图片下载地址  
-def parseDownAdd(preHtml):
-    url = 'https:'+ preHtml
-    html = getHTMLText(url)
-    soup = BeautifulSoup(html,'html.parser')
-    ilt = soup.find_all('img',style="max-width: 728px;max-height: 602px;")
-    print(ilt[0].get('src'))
-    downPic('https:' + ilt[0].get('src'),root)
-
+def parseDownAdd():
+    while True:
+        html = ""
+        n = 5
+        while len(wallPaperList) == 0:
+            time.sleep(500)
+            
+        url = wallPaperList.pop()
+        
+        while html == "":
+            html = getHTMLText(url)
+            --n
+            if(n == 0):
+                wallPaperList.inster(1,url)
+                break
+       # if not html == "":
+        print("2")
+        soup = BeautifulSoup(html,'html.parser')
+        ilt = soup.find_all('img',style="max-width: 728px;max-height: 602px;")
+        print(ilt[0].get('src'))
+        m = 5
+        while downPic('https:' + ilt[0].get('src'),root) == -1:
+            --m
+            if m == 0: 
+                wallPaperList.inster(1,url)
+                break
 
 #下载图片并保存到本地
 def downPic(picURL,root):
@@ -59,21 +97,37 @@ def downPic(picURL,root):
             print("文件已经存在")
     except:
         print("爬取失败")
-
+        return -1
+    return 1
 
 #主函数
 def main():
     start_url = "https://wallpaperscraft.com/all/" + resolutio
+
+    try:
+        #_thread.start_new_thread(parsePage,(a,1))
+        threading.Thread(target = parsePage, args = (), name = 'thread-6').start()
+        #_thread.start_new_thread(parseDownAdd,(c,2)
+        #_thread.start_new_thread(parseDownAdd,(d,3)
+        #_thread.start_new_thread(parseDownAdd,(e,4)
+        for i in range(5):
+            threading.Thread(target = parseDownAdd, args = (), name = 'thread-' + str(i)).start()
+    except:
+        print("Error: unable to start thread")
+    
     for i in range(starPage,endPage+1):
        try:
+           while len(mainPageList) > 5 :
+               time.sleep(500)
            if i == 1:
-               html = getHTMLText(start_url)
+               #html = getHTMLText(start_url)
+               mainPageList.append(start_url)
            else:
                url = start_url + '/page' + str(i)
                print(url)
-               html = getHTMLText(url)
-               
-           parsePage(html)
+               #html = getHTMLText(url)
+               mainPageList.append(url)
+           #parsePage(html)
        except:
             continue
 
